8a4a570050b9eb44dabeb4ab451c1534352813ea
emersonboeira <emerson.boeira@gmail.com>
2019-03-08 13:55:53 -0300

    inv_test: inserting the tests and the criteria of invertibility and stability of the inverse

--- vrft/control.py
+++ vrft/control.py
@@ -53,86 +53,106 @@ def design(u,y1,y2,Td,C,L):
     # transformation of Td from the MIMO transfer function list structure to a state-space model
     Atd,Btd,Ctd,Dtd=vrft.mtf2ss(Td)
     # calculates the virtual reference for the first data set
-    r1v,_=vrft.stbinv(Atd,Btd,Ctd,Dtd,y1.T,t)
+    r1v,_,flagvr=vrft.stbinv(Atd,Btd,Ctd,Dtd,y1.T,t)
     r1v=r1v.T
     # calculates the virtual reference for the second data set (instrumental variable)
-    r2v,_=vrft.stbinv(Atd,Btd,Ctd,Dtd,y2.T,t)
+    r2v,_,_=vrft.stbinv(Atd,Btd,Ctd,Dtd,y2.T,t)
     r2v=r2v.T
-    # remove the last samples of y, to match the dimension of the virtual reference
-    # number of samples used in the method
-    N=r1v.shape[0]
-    y1=y1[0:N,:]
-    y2=y2[0:N,:]
-    # virtual error
-    e1=r1v-y1
-    e2=r2v-y2
-    # remove the last samples of the input (to match the dimension of the virtual error)
-    uf=uf[0:N,:]
-      
-    # calculating the number of parameters of each controller and organizing it as a matrix
-    # preallocating
-    nbpar=np.zeros((n,n))
-    # loop
-    for i in range (0,n):
-        for j in range (0,n):
-            nbpar[i][j]=len(C[i][j])
-            
-    # total number of parameters (casting to integer)
-    p_tot=int(np.sum(nbpar))
     
-    # assembling the matrix phi_N=[phi_1N^T phi_2N^T...phi_nN^T]       
-    # preallocating
-    phi_N=np.zeros((N,p_tot))
-    csi_N=np.zeros((N,p_tot))
-    pacc=0
-    # loop that organizes the matrix phi_N=[phi_1N^T phi_2N^T...phi_nN^T]       
-    for i in range (0,n):
-        for j in range (0,n):
-            if len(C[i][j])>0:
-                # calculating phi_ij(t)^T
-                phi_ij=vrft.filter(C[i][j],e1[:,j:j+1])
-                csi_ij=vrft.filter(C[i][j],e2[:,j:j+1])
-                # number of parameters in Cij(z)
-                pij=int(nbpar[i][j])
-                # assembling phi_N
-                phi_N[:,pacc:pacc+pij]=phi_ij
-                csi_N[:,pacc:pacc+pij]=csi_ij
-                # parameter accumulator
-                pacc=pacc+pij
+    # test if the inversion algorithm was succesful
+    if flagvr==0:
+        # if flagvr=0, then, the inversion algorithm was succesful
+    
+        # remove the last samples of y, to match the dimension of the virtual reference
+        # number of samples used in the method
+        N=r1v.shape[0]
+        y1=y1[0:N,:]
+        y2=y2[0:N,:]
+        # virtual error
+        e1=r1v-y1
+        e2=r2v-y2
+        # remove the last samples of the input (to match the dimension of the virtual error)
+        uf=uf[0:N,:]
+        
+        # calculating the number of parameters of each controller and organizing it as a matrix
+        # preallocating
+        nbpar=np.zeros((n,n))
+        # loop
+        for i in range (0,n):
+            for j in range (0,n):
+                nbpar[i][j]=len(C[i][j])
+            
+        # total number of parameters (casting to integer)
+        p_tot=int(np.sum(nbpar))
+    
+        # assembling the matrix phi_N=[phi_1N^T phi_2N^T...phi_nN^T]       
+        # preallocating
+        phi_N=np.zeros((N,p_tot))
+        csi_N=np.zeros((N,p_tot))
+        pacc=0
+        # loop that organizes the matrix phi_N=[phi_1N^T phi_2N^T...phi_nN^T]       
+        for i in range (0,n):
+            for j in range (0,n):
+                if len(C[i][j])>0:
+                    # calculating phi_ij(t)^T
+                    phi_ij=vrft.filter(C[i][j],e1[:,j:j+1])
+                    csi_ij=vrft.filter(C[i][j],e2[:,j:j+1])
+                    # number of parameters in Cij(z)
+                    pij=int(nbpar[i][j])
+                    # assembling phi_N
+                    phi_N[:,pacc:pacc+pij]=phi_ij
+                    csi_N[:,pacc:pacc+pij]=csi_ij
+                    # parameter accumulator
+                    pacc=pacc+pij
                 
-    # using the accumulator again
-    pacc=0                
-    # loop that organize the phi_vrf matrix (with the filter)
-    # preallocating
-    phivrf=np.zeros((N*n,p_tot))
-    csivrf=np.zeros((N*n,p_tot))
-    # loop
-    for i in range (0,n):
-        for j in range (0,n):
-            # number of parameters regarding the signal ui:
-            p_i=int(np.sum(nbpar[i])) # casting to integer as well
-            # separates the phi_iN signal
-            phi_iN=phi_N[:,pacc:pacc+p_i]
-            csi_iN=csi_N[:,pacc:pacc+p_i]
-            # using the MIMO filter L(q)
-            phivrf[N*j:N*(j+1),pacc:pacc+p_i]=vrft.filtra_all( L[j][i],phi_iN ) # the index are correct, despite the inversion :)
-            csivrf[N*j:N*(j+1),pacc:pacc+p_i]=vrft.filtra_all( L[j][i],csi_iN ) # instrumental variable
-        # acumulating the parameters
-        pacc=pacc+p_i
+        # using the accumulator again
+        pacc=0                
+        # loop that organize the phi_vrf matrix (with the filter)
+        # preallocating
+        phivrf=np.zeros((N*n,p_tot))
+        csivrf=np.zeros((N*n,p_tot))
+        # loop
+        for i in range (0,n):
+            for j in range (0,n):
+                # number of parameters regarding the signal ui:
+                p_i=int(np.sum(nbpar[i])) # casting to integer as well
+                # separates the phi_iN signal
+                phi_iN=phi_N[:,pacc:pacc+p_i]
+                csi_iN=csi_N[:,pacc:pacc+p_i]
+                # using the MIMO filter L(q)
+                phivrf[N*j:N*(j+1),pacc:pacc+p_i]=vrft.filtra_all( L[j][i],phi_iN ) # the index are correct, despite the inversion :)
+                csivrf[N*j:N*(j+1),pacc:pacc+p_i]=vrft.filtra_all( L[j][i],csi_iN ) # instrumental variable
+            # acumulating the parameters
+            pacc=pacc+p_i
         
-    # reorganizing the uf vector (stacking)
-    # preallocating
-    Uf=np.zeros((N*n,1))
-    # loop
-    for i in range (0,n):
-        Uf[N*i:N*(i+1)]=uf[:,i:i+1]
+        # reorganizing the uf vector (stacking)
+        # preallocating
+        Uf=np.zeros((N*n,1))
+        # loop
+        for i in range (0,n):
+            Uf[N*i:N*(i+1)]=uf[:,i:i+1]
         
-    # compute parameters
-    Z=csivrf.T@phivrf
-    Y=csivrf.T@Uf
-    p=np.linalg.inv(Z)@Y
-
-    return p
+        # compute parameters
+        Z=csivrf.T@phivrf
+        Y=csivrf.T@Uf
+        p=np.linalg.inv(Z)@Y
+        
+        # returning the parameter vector
+        return p
+    
+    elif flagvr==1:
+        # if flagvr=1, then, it was not possible to calculate the inverse of the reference model
+        print("It was not possible to calculate the virtual reference.")
+        # return an empty parameter vector
+        p=np.empty((0,0))
+        return p
+        
+    elif flagvr==2:
+        # if flagvr=2, the inverse of the reference model is unstable. VRFT method aborted!
+        print("The inverse of the reference model Td(z) is unstable. It is not recommended to proceed with the VRFT method, so the algorithm was aborted!")
+        # return an empty parameter vector
+        p=np.empty((0,0))
+        return p
     
     # Filter Signals 
     # E1 normal experiment
