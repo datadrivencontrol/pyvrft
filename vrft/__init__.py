6ffc986f557083021d7d97bb3a52a4f9a4720031
Diego Eckhard <diegoeck@ufrgs.br>
2019-05-29 12:15:57 -0300

    master: Remove functions

--- vrft/__init__.py
+++ vrft/__init__.py
@@ -1,6 +1,5 @@
 from vrft.control import design
 from vrft.control import filter
-from vrft.control import colfilter
 from vrft.invfunc import stbinv
 from vrft.invfunc import mtf2ss
 from vrft.csvfunc import datafromcsv
